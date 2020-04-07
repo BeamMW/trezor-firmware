28387c9b6d3c0e7c240aa5af356daea9b9cd74d2
Septen <gammerxpower@gmail.com>
2020-04-08 01:36:27 +0300

    Replaced beam crpypto code with beam-hw-lib submodule.

--- crypto/beam/README.md
+++
@@ -1,2 +0,0 @@
-## Some parts of the library come from external sources:
-- C Vec implementation: https://github.com/rxi/vec
--- crypto/beam/beam.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TREZOR_CRYPTO_BEAM_H__
-#define __TREZOR_CRYPTO_BEAM_H__
-
-#if !USE_BEAM
-#error "Compile with -DUSE_BEAM=1"
-#endif
-
-// __TREZOR_CRYPTO_BEAM_H__
-#endif
