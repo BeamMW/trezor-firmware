ed621345fc9fbe6a45c0f354e09e370dbfe7b127
Septen <gammerxpower@gmail.com>
2020-05-07 04:56:44 +0300

    GetNumSlots firmware fixes. UX: WIP.

--- core/src/apps/beam/get_num_slots.py
+++ core/src/apps/beam/get_num_slots.py
@@ -4,7 +4,7 @@ from trezor.messages.BeamNumSlots import BeamNumSlots
 from trezor.messages.Failure import Failure
 
 from apps.beam.nonce import is_master_nonce_created
-from apps.beam.layout import beam_confirm_message
+#from apps.beam.layout import beam_confirm_message
 
 
 async def get_num_slots(ctx, msg):
@@ -15,7 +15,15 @@ async def get_num_slots(ctx, msg):
 
     num_slots = __get_num_nonce_slots()
     if msg.show_display:
-        await beam_confirm_message(ctx, "Nonce slots", "Number of available nonce slots is " + str(num_slots), False)
+        from apps.common.confirm import require_confirm
+        from trezor.messages.ButtonRequestType import ProtectCall
+        from trezor import ui
+        from trezor.ui.text import Text
+
+        text = Text("Nonce slots", ui.ICON_SEND, ui.GREEN, new_lines=False)
+        text.normal(ui.GREY, "Number of available nonce slots is:", ui.FG)
+        text.bold(str(num_slots))
+        await require_confirm(ctx, text, ProtectCall)
 
     return BeamNumSlots(num_slots=num_slots)
 
